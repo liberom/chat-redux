6e58306fdf580037559e0c19953ac76ee28e2634
Vinicius Libero <libero.moreira@gmail.com>
2021-03-25 17:27:55 -0300

    added actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,34 @@
 // TODO: add and export your own actions
+
+export const LOAD_CHANNELS = 'LOAD_CHANNELS'
+export const LOAD_MESSAGES = 'LOAD_MESSAGES'
+export const SET_ACTIVE_CHANNEL = 'SET_ACTIVE_CHANNEL'
+export const POST_MESSAGE = 'POST_MESSAGE'
+
+export function loadChannels() {
+  return {
+    type: LOAD_CHANNELS,
+    payload: channels
+  };
+}
+
+export function loadMessages() {
+  return {
+    type: LOAD_MESSAGES,
+    payload: messages
+  };
+}
+
+export function setActiveChannel(channel) {
+  return {
+    type: SET_ACTIVE_CHANNEL,
+    payload: channel
+  };
+}
+
+export function postMessage(message) {
+  return {
+    type: POST_MESSAGE,
+    payload: message
+  };
+}
