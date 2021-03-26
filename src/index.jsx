f5f10b811a975229a18bef9218607ddd4918e783
Vinicius Libero <libero.moreira@gmail.com>
2021-03-26 10:55:27 -0300

    app completed

--- src/index.jsx
+++ src/index.jsx
@@ -1,53 +1,45 @@
+
+/* eslint no-alert:off */
+
 // external modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { logger } from 'redux-logger';
+import logger from 'redux-logger';
 import reduxPromise from 'redux-promise';
-
-import channelsReducer from './reducers/channels_reducer';
-import messagesReducer from './reducers/messages_reducer';
-import activeChannelReducer from './reducers/active_channel_reducer';
-import usernameReducer from './reducers/username_reducer';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
 // State and reducers
+import messagesReducer from './reducers/messages_reducer';
+import selectedChannelReducer from './reducers/selected_channel_reducer';
+
 const identityReducer = (state = null) => state;
 
 const initialState = {
-  messages: [
-    {
-      "author":"anonymous92",
-      "content":"Hello world!",
-      "created_at":"2017-09-26T23:03:16.365Z"
-    },
-    {
-      "author":"anonymous77",
-      "content":"My name is anonymous77",
-      "created_at":"2017-09-26T23:03:21.194Z"
-    }
-  ],
-  channels: [ 'general', 'react', 'paris' ],
+  messages: [],
+  channels: ['general', 'react', 'paris'],
   currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
-  activeChannel: 'general'
+  selectedChannel: 'general'
 };
 
 const reducers = combineReducers({
-  channels: identityReducer,
   messages: messagesReducer,
-  activeChannel: activeChannelReducer,
-  currentUser: identityReducer
+  channels: identityReducer,
+  currentUser: identityReducer,
+  selectedChannel: selectedChannelReducer
 });
 
+// Middlewares
 const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, initialState, middlewares)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
