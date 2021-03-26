08a13ad9f47f79695ad251c7847915754f99a4d4
Vinicius Libero <libero.moreira@gmail.com>
2021-03-26 10:37:31 -0300

    fixed channel list

--- src/containers/channel_list.jsx
+++ src/containers/channel_list.jsx
@@ -3,17 +3,42 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import Channel from './channel';
-import { loadChannels } from '../actions';
+// import { loadChannels } from '../actions';
+import { selectChannel, leadMessages } from '../actions/index';
 
 // Component or Container and Action
 
 class ChannelList extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.activeChannel !== this.props.activeChannel) {
+      this.props.loadMessages(nextProps.activeChannel);
+    }
+  }
+
+  handleClick = (channel) => {
+    this.props.selectChannel(channel);
+  }
+
+  renderChannel = (channel) => {
+    return (
+      <li
+        key={channel}
+        className={channel === this.props.selectedChannel ? 'active' : null}
+        onClick={() => this.handleClick(channel)}
+        role="presentation"
+      >
+        #{channel}
+      </li>
+    );
+  }
+
   render() {
     return (
-      <div className="channels" >
-        {this.props.channels.map((channel) => {
-          return <Channel key={channel.name} channel={channel} />
-        })}
+      <div className="channels-container" >
+      <span>Redux Chat</span>
+        <ul>
+          {this.props.channels.map(this.renderChannel)}
+        </ul>
 
       </div>
     );
@@ -23,13 +48,14 @@ class ChannelList extends Component {
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(
-    { loadChannels: loadChannels }, dispatch
+    { selectChannel, loadChannels }, dispatch
   );
 }
 
 function mapStateToProps(state) {
   return {
-    channels: state.channels
+    channels: state.channels,
+    activeChannel: state.activeChannel
   };
 }
 
