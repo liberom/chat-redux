ea0ed98290b22d52f4be51b382c879f5370a12e7
Vinicius Libero <libero.moreira@gmail.com>
2021-03-25 21:42:30 -0300

    edited message form

--- src/containers/message_form.jsx
+++ src/containers/message_form.jsx
@@ -3,29 +3,58 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 // Component or Container and Action
+import { postMessage } from '../actions';
 
 class MessageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  componentDidMount() {
+    this.messageBox.focus();
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.postMessage(this.props.activeChannel, this.props.currentUser, this.state.value);
+    this.setState({ value: '' }); // Reset message input
+  }
+
   render() {
     return (
-      <div className='message-form'>
-        <input type="text" />
-        <button>Send</button>
-      </div>
+      <form className="channel-editor" onSubmit={this.handleSubmit}>
+        <input
+          ref={(input) => { this.messageBox = input; }}
+          type="text"
+          className="form-control"
+          autoComplete="off"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button type="submit">Send</button>
+      </form>
+
     );
   }
 
 }
 
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators(
-//     // { setCities: setCities }, dispatch
-//   );
-// }
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { postMessage }, dispatch
+  );
+}
 
-// function mapStateToProps(state) {
-//   return {
-//     // cities: state.cities
-//   };
-// }
+function mapStateToProps(state) {
+  return {
+    activeChannel: state.activeChannel,
+    currentUser: state.currentUser
+  };
+}
 
-export default (MessageForm);
+export default connect(mapStateToProps, mapDispatchToProps)(MessageForm);
