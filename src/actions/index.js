8ccb65727098d14f70473cba54822816f0250422
Vinicius Libero <libero.moreira@gmail.com>
2021-03-26 10:43:00 -0300

    fixed actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,38 +1,21 @@
+
 const BASE_URL = 'https://wagon-chat.herokuapp.com';
 
-export const LOAD_CHANNELS = 'LOAD_CHANNELS';
-export const LOAD_MESSAGES = 'LOAD_MESSAGES';
-export const SET_ACTIVE_CHANNEL = 'SET_ACTIVE_CHANNEL';
-export const POST_MESSAGE = 'POST_MESSAGE';
-export const SET_USERNAME = 'SET_USERNAME';
+export const FETCH_MESSAGES = 'FETCH_MESSAGES';
+export const MESSAGE_POSTED = 'MESSAGE_POSTED';
+export const CHANNEL_SELECTED = 'CHANNEL_SELECTED';
 
-export function loadChannels() {
+export function fetchMessages(channel) {
+  const url = `${BASE_URL}/${channel}/messages`;
+  const promise = fetch(url).then(r => r.json());
+
   return {
-    type: LOAD_CHANNELS,
-    payload: channels
+    type: FETCH_MESSAGES,
+    payload: promise // Will be resolved by redux-promise
   };
 }
 
-export function loadMessages(channel) {
-
-   return fetch(`https://wagon-chat.herokuapp.com/${channel}/messages`)
-   .then(response => response.json())
-   .then((data) => {
-     return {
-       type: LOAD_MESSAGES,
-       payload: data.messages
-     };
-   });
-}
-
-export function setActiveChannel(channel) {
-  return {
-    type: SET_ACTIVE_CHANNEL,
-    payload: channel
-  };
-}
-
-export function postMessage(channel, author, content) {
+export function createMessage(channel, author, content) {
   const url = `${BASE_URL}/${channel}/messages`;
   const body = { author, content }; // ES6 destructuring
   const promise = fetch(url, {
@@ -45,14 +28,14 @@ export function postMessage(channel, author, content) {
   }).then(r => r.json());
 
   return {
-    type: POST_MESSAGE,
-    payload: promise
+    type: MESSAGE_POSTED,
+    payload: promise // Will be resolved by redux-promise
   };
 }
 
-export function setUsername(username = prompt("What's your username?")) {
+export function selectChannel(channel) {
   return {
-    type: SET_USERNAME,
-    payload: username
+    type: CHANNEL_SELECTED,
+    payload: channel
   };
 }
--- src/reducers/messages_reducer.js
+++ src/reducers/messages_reducer.js
@@ -1,12 +1,18 @@
-import { LOAD_MESSAGES } from '../actions';
+import { FETCH_MESSAGES, MESSAGE_POSTED, CHANNEL_SELECTED } from '../actions';
 
-export default function(state, action) {
-  if (state === undefined) {
-    return [];
-  }
+export default function(state = null, action) {
   switch (action.type) {
-    case LOAD_MESSAGES:
-      return action.payload;
+    case FETCH_MESSAGES: {
+      return action.payload.messages;
+    }
+    case MESSAGE_POSTED: {
+      const copiedState = state.slice(0);
+      copiedState.push(action.payload);
+      return copiedState;
+    }
+    case CHANNEL_SELECTED: {
+      return []; // Channel has changed. Clearing view.
+    }
     default:
       return state;
   }
